57000b0e5fdf8ffad846e17be43750754f979f05
f056917 <33644849+f056917@users.noreply.github.com>
2018-02-28 09:37:42 +0800

    2018.2.28-0937

--- JS/test/test.js
+++ JS/test/test.js
@@ -1,2 +1,22 @@
-var test = "This is the letter sigma: \u03a3.";
-alert(test);
+// var obj = {
+//     toString: function () {
+//         return 1;
+//     }
+// };
+// var obj = new Date();
+// obj.valueOf = function () { return 1 };
+// obj.toString = function () { return "hello" };
+// var age = obj + 2;
+// alert(age);
+// if(1>2) {
+//     alert("1>2");
+// }else {
+//     alert("条件不足");
+// }
+var test = document.getElementById('test');
+test.addEventListener('mouseenter',function (event) {
+    event.target.style.color = 'purple';
+    setTimeout(function () {
+        event.target.style.color = '';
+    },500);
+},false);
