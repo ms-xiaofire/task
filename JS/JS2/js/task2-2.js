36e7c3d66e70e8fec5cb2eb9b8da277d8c2c4d78
f056917 <33644849+f056917@users.noreply.github.com>
2018-03-06 20:29:37 +0800

    2018.3.6-2028

--- JS/JS2/js/task2-2.js
+++ JS/JS2/js/task2-2.js
@@ -2,22 +2,31 @@
 function back() {
     window.location.href="task2-1.html";
 }
-//输入框输入人数
-function set() {
-    var a = document.getElementById("num").value;
-    if(a>3&&a<19){
-        alert(a);
-    }else {
-        alert("请输入正确的玩家人数!")
-    }
-}
-//同步输入框和滑动条两个input
-var o_1 = document.getElementById("num");
-var o_2 = document.getElementById("progress");
-o_1.onkeyup = function () {
-    o_1.value = o_2.value;
-};
-// function num() {
-//     var b = document.getElementsByName("number")[0].value;
-//     document.getElementsByName("number")[1].value = b;
+$(function s1() {
+
+    $("#slider2").slider({ max: 18, min: 4, step: 1, change: function s1(e, ui) { $('#C_Rejection').val(ui.value.toFixed(1)); }, value: parseFloat($('#C_Rejection').val()) || 0/*初始化值，不设置默认为0*/ });
+    $("#slider2").closest('td').find('input[type="button"]').click(function s1() {
+        var v = $("#slider2").slider('value'), opt = $("#slider2").slider('option');
+        v = eval(v + this.value + opt.step);
+        if (v < opt.min) v = opt.min; else if (v > opt.max) v = opt.max;
+        $("#slider2").slider('value', v);
+        $('#C_Rejection').val(v.toFixed(1)); //浮点计算js可能会导致精度出错，需要toFixed转下精度
+    });
+});
+
+// //输入框输入人数
+// function set() {
+//     var a = document.getElementById("num").value;
+//     if(a>3&&a<19){
+//         alert(a);
+//     }else {
+//         alert("请输入正确的玩家人数!")
+//     }
 // }
+// //同步输入框和滑动条
+// var o_1 = document.getElementById("num");
+// var o_2 = document.getElementById("progress");
+// o_1.onkeyup = function () {
+//     o_2.value = o_1.value;
+// };
+
