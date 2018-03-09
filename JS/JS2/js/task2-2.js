efcfd68428a6ba840e0500e5797d1956a97756e6
f056917 <33644849+f056917@users.noreply.github.com>
2018-03-09 10:26:26 +0800

    2018.3.9-1026

--- JS/JS2/js/task2-2.js
+++ JS/JS2/js/task2-2.js
@@ -2,18 +2,10 @@
 function back() {
     window.location.href="task2-1.html";
 }
-//输入框输入人数
-function set() {
-    var a = document.getElementById("C_Rejection").value;
-    if(a>3&&a<19){
-        alert(a);
-    }else {
-        alert("请输入正确的玩家人数!")
-    }
-}
+
 //同步输入框和滑动条
 $(function s1() {
-    $("#slider2").slider({ max: 18, min: 4, step: 1, change: function s1(e, ui) { $('#C_Rejection').val(ui.value.toFixed(1)); }, value: parseFloat($('#C_Rejection').val()) || 0/*初始化值，不设置默认为0*/ });
+    $("#slider2").slider({ max: 18, min: 4, step: 1, change: function s1(e, ui) { $('#C_Rejection').val(ui.value.toFixed(1)); }, value: parseInt($('#C_Rejection').val()) || 0/*初始化值，不设置默认为0*/ });
     $("#slider2").closest('td').find('input[type="button"]').click(function s1() {
         var v = $("#slider2").slider('value'), opt = $("#slider2").slider('option');
         v = eval(v + this.value + opt.step);
@@ -23,10 +15,18 @@ $(function s1() {
     });
 });
 
-// //同步输入框和滑动条
-// var o_1 = document.getElementById("num");
-// var o_2 = document.getElementById("progress");
-// o_1.onkeyup = function () {
-//     o_2.value = o_1.value;
-// };
+//分析杀手人数和平民人数
+function set() {
+    var a = document.getElementById("C_Rejection").value;
+    var k = document.getElementById("kill");
+    var c = document.getElementById("civilian");
+    console.log(a);
+    if(a>3&&a<19) {
+        k.innerHTML = Math.floor(a/4);
+        c.innerHTML = a-k.innerHTML;
+        var killA =k.innerHTML;
+        var Num = [];
+    }else alert("请输入正确的玩家数量!");
+}
+
 
--- JS/test/test.js
+++ JS/test/test.js
@@ -1,30 +1,9 @@
-(function(){
-    var range = document.getElementById("range"),
-        volume = document.forms[0]["volume"];//第一次使用这种方式获取document.forms[index][formName];
-    //设置初值
-    cachedRangeValue = localStorage.rangeValue ? localStorage.rangeValue : 5;
-    range.value = volume.value =  cachedRangeValue;
-    //判断浏览器是否支持range滑动条
-    if(range.type == "text"){
-        document.forms[0].innerHTML = "抱歉，您的浏览器不支持range滑动条，请使用高版本浏览器";
-    }
-    //默认"false"，事件句柄在冒泡阶段支持；若为"true"，则事件在句柄捕获阶段执行
-    range.addEventListener("mouseup",function(){
-        localStorage ? (localStorage.rangeValue = range.value) : (console.log("不支持本地存储"));
-    },"false");
-    if(range.addEventListener){
-        range.addEventListener("mouseup",function(){
-            localStorage ? (localStorage.rangeValue = range.value) : (console.log("不支持本地存储"));
-        },"false");
-        range.addEventListener("change",function(){
-            volume.value = range.value;
-        });
-    }else{
-        range.attachEvent("onmouseup",function(){
-            localStorage ? (localStorage.rangeValue = range.value) : (console.log("不支持本地存储"));
-        });
-        range.attachEvent("change",function(){
-            volume.value = range.value;
-        })
-    }
-})();
+var tom = Array(3);
+tom[0] = "name";
+tom[1] = "age";
+tom[2] = "sex";
+tom[3] = "job";
+
+function set() {
+    alert(tom[3]);
+}
