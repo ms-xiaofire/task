a0b3b6b07e04af1b0cc351a6443952203b6691b8
f056917 <33644849+f056917@users.noreply.github.com>
2018-03-12 09:14:33 +0800

    2018.3.12-0914

--- JS/JS2/js/task2-2.js
+++ JS/JS2/js/task2-2.js
@@ -1,17 +1,26 @@
 //返回首页
 function back() {
-    window.location.href="task2-1.html";
+    window.location.href = "task2-1.html";
 }
 
 //同步输入框和滑动条
 $(function s1() {
-    $("#slider2").slider({ max: 18, min: 4, step: 1, change: function s1(e, ui) { $('#C_Rejection').val(ui.value.toFixed(1)); }, value: parseInt($('#C_Rejection').val()) || 0/*初始化值，不设置默认为0*/ });
+    $("#slider2").slider({
+        max: 18,
+        min: 4,
+        step: 1,
+        change: function s1(e, ui) {
+            $('#C_Rejection').val(ui.value.toFixed(0));
+        },
+        value: parseInt($('#C_Rejection').val()) || 0/*初始化值，不设置默认为0*/
+    });
     $("#slider2").closest('td').find('input[type="button"]').click(function s1() {
         var v = $("#slider2").slider('value'), opt = $("#slider2").slider('option');
         v = eval(v + this.value + opt.step);
         if (v < opt.min) v = opt.min; else if (v > opt.max) v = opt.max;
         $("#slider2").slider('value', v);
-        $('#C_Rejection').val(v.toFixed(1)); //浮点计算js可能会导致精度出错，需要toFixed转下精度
+        console.log(v);
+        $('#C_Rejection').val(v.toFixed(0)); //浮点计算js可能会导致精度出错，需要toFixed转下精度
     });
 });
 
@@ -21,31 +30,34 @@ function set() {
     var k = document.getElementById("kill");
     var c = document.getElementById("civilian");
     var l = "";
-    console.log(a);
-    if(a>3&&a<19) {
-        k.innerHTML = Math.floor(a/4);
-        c.innerHTML = a-k.innerHTML;
-        var killA = Math.floor(a/4);
+    //验证玩家数量
+    if (a > 3 && a < 19) {
+        k.innerHTML = Math.floor(a / 4);
+        c.innerHTML = a - k.innerHTML;
+        var killA = Math.floor(a / 4);
         var totalArr = [];
-
-        for(var n=0;n<killA;n++){
+        //遍历杀手数量
+        for (var n = 0; n < killA; n++) {
             totalArr[n] = "杀手";
         }
-        for(n=killA;n<a;n++){
+        //遍历平民数量
+        for (n = killA; n < a; n++) {
             totalArr[n] = "平民";
         }
+        //数组乱序,把平民和杀手的顺序打乱
         totalArr.sort(function () {
             return 0.5 - Math.random()
         });
-
-        for(var i=0;i<a;i++){
+        //把最终平民和杀手的位置输出到id为per的div里
+        for (var i = 0; i < a; i++) {
             var m = i + 1;
             l += m + '号' + totalArr[i] + "</br>";
         }
         document.getElementById("per").innerHTML = l;
 
 
-    }else alert("请输入正确的玩家数量!");
+    } else alert("请输入正确的玩家数量!");
 }
-
-
+function star() {
+    window.location.href = "task2-3.html";
+}
