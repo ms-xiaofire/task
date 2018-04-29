bbec6f06f6c19ba64f590555119d0bd3d4ce91a6
f056917 <33644849+f056917@users.noreply.github.com>
2018-04-29 11:40:12 +0800

    201804029-1139

--- JS/JS6/js/app.js
+++ JS/JS6/js/app.js
@@ -1,9 +1,36 @@
 
 
-angular.module("routeApp", ['ngRoute']).config(['$routeProvider', function ($routeProvider) {
-    $routeProvider
-        .when('/login', {templateUrl: 'login.html'})
-        .when('/list', {templateUrl: 'list.html'})
-        .otherwise({redirectTo: '/login'});
-}]);
+// angular.module("routeApp", ['ngRoute']).config(['$routeProvider', function ($routeProvider) {
+//     $routeProvider
+//         .when('/login', {templateUrl: 'login.html'})
+//         .when('/list', {templateUrl: 'list.html'})
+//         .otherwise({redirectTo: '/login'});
+// }]);
+
+
+
+angular.module("App", ['ui.router'])
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/login');
+        $stateProvider
+            //登录页面
+            .state('login', {
+                url: '/login',
+                templateUrl: 'login.html'
+            })
+            //列表页面
+            .state('list', {
+                url: '/list',
+                templateUrl: 'list.html'
+            })
+            //列表欢迎页面
+            .state('list.welcome', {
+                url: '/welcome',
+                templateUrl: 'welcome.html'
+            })
+            .state('list.article', {
+                url: '/article',
+                templateUrl: 'article.html'
+            })
+    });
 
