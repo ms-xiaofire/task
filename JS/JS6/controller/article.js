f0eb6cc0dc8c8bac07716b1e3a56b08bd95ed7cf
f056917 <33644849+f056917@users.noreply.github.com>
2018-05-19 10:15:19 +0800

    20180503-1819

--- JS/JS6/controller/article.js
+++ JS/JS6/controller/article.js
@@ -1,13 +1,50 @@
 
 //article列表控制器
 angular.module('App')
-.controller('artCtrl', ['$scope', '$http', '$state', function ($scope, $http, $state) {
+.controller('artCtrl', ['$scope', '$http', '$state', function ($scope, $http, $state, $stateParams) {
+    $scope.searchParams = $stateParams;
+
+    //get article列表
+    $http({
+        method: 'GET',
+        url: '/carrots-admin-ajax/a/article/search',
+        params: $scope.params
+        //请求成功执行的代码
+    }).then(function successCallback(response) {
+        if(response.data.code===0){
+            console.log(response);
+            //article列表
+            $scope.lists = response.data.data.articleList;
+            $scope.total = response.data.data.total;
+            //分页
+            $scope.totalItems = 64;
+            $scope.currentPage = 4;
+
+            $scope.maxSize = 5;
+            $scope.bigTotalItems = $scope.total;
+            $scope.bigCurrentPage = 1;
+
+            $scope.page = function(page) {
+                $scope.bigCurrentPage = page;
+                $http({
+                    method: 'GET',
+                    params: {page:$scope.bigCurrentPage},
+                    url: '/carrots-admin-ajax/a/article/search'
+                    //请求成功执行的代码
+                }).then(function successCallback(response) {
+                    $scope.lists = response.data.data.articleList;
+                })
+            };
+        }
+        // 请求失败执行的代码
+    },function errorCallback(response) {
+        console.log(response);
+    });
+
     //日期插件
-    // $scope.dat = new Date();
-    $scope.dat = '';
-    $scope.dat_ = '';
-    $scope.enddat = '';
+    //格式化日期
     $scope.format = 'yyyy/MM/dd';
+    //手动输入日期的格式
     $scope.altInputFormats = ['yyyy/M!/d!'];
 
     $scope.popup1 = {
@@ -23,49 +60,29 @@ angular.module('App')
         $scope.popup2.opened = true;
     };
 
-    //按日期搜索
+    $scope.params = {};
+    //搜索
+    $scope.search = function () {
 
-
-    //getarticle列表
-    $http({
-        method: 'GET',
-        url: '/carrots-admin-ajax/a/article/search'
-        //请求成功执行的代码
-    }).then(function successCallback(response) {
-        if(response.data.code===0){
-            console.log(response);
-            //article列表
-            $scope.lists = response.data.data.articleList;
-
-            //分页
-            //总数
-            $scope.totalItems = response.data.data.total;
-            //每页显示数量
-            $scope.iteamsPerpage = 10;
-            //实际页数
-            $scope.totalPage = Math.ceil($scope.totalItems/$scope.iteamsPerpage);
-            //最大页数
-            $scope.maxSize = 10;
-            //当前页
-            $scope.currentPage = '';
-            console.log($scope.currentPage);
-            //页数变化时执行的函数
-            $scope.page = function (page) {
-                $scope.currentPage = page;
-                console.log($scope.currentPage);
-
-                $http({
-                    method: 'GET',
-                    url: '/carrots-admin-ajax/a/article/search?page='+$scope.currentPage
-                    //请求成功执行的代码
-                }).then(function successCallback(response) {
-                    $scope.lists = response.data.data.articleList;
-                })
-            }
+        if($scope.params.startAt){
+            $scope.params.startAt = $scope.params.startAt.valueOf();
         }
-        // 请求失败执行的代码
-    },function errorCallback(response) {
-        console.log(response);
-    });
+        if($scope.params.endAt){
+            $scope.params.endAt = $scope.params.endAt.valueOf();
+        }
+        $http({
+            method: 'GET',
+            url: '/carrots-admin-ajax/a/article/search',
+            params: $scope.params
+            //请求成功执行的代码
+        }).then(function successCallback(response) {
+            $scope.lists = response.data.data.articleList;
+        })
+    };
+
+    //清空
+    // $scope.clear = function () {
+    //
+    // };
 }]);
 
