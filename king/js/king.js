a77a80064a1dd4ed5080d4ccc1e432a81ba577fa
f056917 <33644849+f056917@users.noreply.github.com>
2018-08-08 12:22:41 +0800

    20180808-1222-少儿编程

--- king/js/king.js
+++ king/js/king.js
@@ -1,5 +1,8 @@
 $(document).ready(function () {
     $('.carousel').carousel({
         interval: 3000
+    });
+    $('#king').click(function () {
+        window.location.reload('king.html')
     })
-})
+});
